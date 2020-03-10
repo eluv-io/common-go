19748e0104ed00a83b002191d25c3eaff8b65547
elv-gilles <40674218+elv-gilles@users.noreply.github.com>
2020-03-10 11:38:01 +0100

    Cmd upload multiple files (#907)
    * fabric - files upload: write file chunks by encrypted blocks to allow resuming
    * qfab_cli - files upload
       * cleanup commands and output
       * add multi-file upload  (fixes #879)
    * encrypt
       * cleanup conk encrypt/decrypt naming
       * fail encrypting read seek if seek not at block boundary
       * fail re-encryption if size of the encrypted block does not match the header
       * concrypt/reader : raise error if delegateBlockTransformer has more bytes than expected
    * qfab_cli - kms commands:
       * space kms list must just return empty list if not found (no error)
       * library kms commands were forgotten (exist in 'cmd')
    * qfab_cli 
       * report already accomplished steps in case of error or exit signal
       * add 'show-steps' flag to (force) show intermediary results
       * add 'debug' flag to simplify using debug level
       * hide uncommon 'global' flags
       * split flags validation in two steps 
       * content create: no error if encrypt and content has no kms
       * disable encryption for ingest files by reference
    * avtesting.TestDeployToAndAccessRunningFabric use environment variables

---
+++ util/fileutil/walk.go
@@ -0,0 +1,63 @@
+package fileutil
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/qluvio/content-fabric/errors"
+)
+
+// Walk implements filepath.Walk but allows to also resolve symlinks
+func Walk(path string, followSymlinks bool, walkFn filepath.WalkFunc) error {
+	if !followSymlinks {
+		return filepath.Walk(path, walkFn)
+	}
+	return walk(path, path, walkFn, nil)
+}
+
+// walk walks the given walkPath.
+//
+//  * walkPath: path with which the walk starts
+//  * linkPath: the path of the directory symlink that triggered this walk, or
+//              same as walkPath for original walk
+//  * walkFn:   the function called for all found files and directories
+//  * visited:  list of walkPaths already visited
+func walk(walkPath string, linkPath string, walkFn filepath.WalkFunc, visited []string) error {
+	e := errors.Template("fileutil.WalkSym", "walk_path", walkPath)
+	for _, v := range visited {
+		if strings.HasPrefix(walkPath, v) {
+			return e(errors.K.Invalid,
+				"reason", "symlink loop detected",
+				"symlink", linkPath,
+				"already_visited", v)
+		}
+	}
+	visited = append(visited, walkPath)
+
+	symWalkFunc := func(path string, info os.FileInfo, err error) error {
+		e := e.Add("path", path)
+		if fname, err := filepath.Rel(walkPath, path); err == nil {
+			path = filepath.Join(linkPath, fname)
+		} else {
+			return e(err)
+		}
+
+		if err == nil && info.Mode()&os.ModeSymlink == os.ModeSymlink {
+			finalPath, err := filepath.EvalSymlinks(path)
+			if err != nil {
+				return e(err)
+			}
+			info, err = os.Lstat(finalPath)
+			if err != nil {
+				return walkFn(path, info, err)
+			}
+			if info.IsDir() {
+				return walk(finalPath, path, walkFn, visited)
+			}
+		}
+
+		return walkFn(path, info, err)
+	}
+	return e.IfNotNil(filepath.Walk(walkPath, symWalkFunc))
+}
