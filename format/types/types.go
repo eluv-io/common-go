c852a778e647cbe687bbadfcd2f10663247fc7f6
janfalkin <jan.falkin@eluv.io>
2020-06-27 18:15:08 -0700

    initial impl for search terms with or's content ids

--- format/types/types.go
+++ format/types/types.go
@@ -19,6 +19,9 @@ type AccountID = id.ID
 // UserID is the type of user IDs
 type UserID = id.ID
 
+// GroupID is the type of group IDs
+type GroupID = id.ID
+
 // QLibID is the type of content library IDs
 type QLibID = id.ID
 
