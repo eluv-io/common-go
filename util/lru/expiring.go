65f90e3fb4c5273e17c81c29ee635c439b65aad3
elv-gilles <40674218+elv-gilles@users.noreply.github.com>
2021-02-01 18:32:42 +0100

    auth: add cache for user access  (#1622)
    * auth: add cache for user access (configuration in BcAuthConfig)
    * config: auth key cache and content cache now use expiring cache config

--- util/lru/expiring.go
+++ util/lru/expiring.go
@@ -35,6 +35,11 @@ func (c *ExpiringCache) GetOrCreate(
 	constructor func() (interface{}, error),
 	evict ...func(val interface{}) bool) (val interface{}, evicted bool, err error) {
 
+	if c == nil {
+		val, err = constructor()
+		return val, false, err
+	}
+
 	val, evicted, err = c.cache.GetOrCreate(
 		key,
 		func() (interface{}, error) {
