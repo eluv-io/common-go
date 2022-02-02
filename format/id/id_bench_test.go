9b371bb2299d6903ed6296fc952b0321e3a5b84e
Lukas Anliker <lukas.anliker@eluv.io>
2022-02-02 11:33:19 +0100

    code review

--- format/id/id_bench_test.go
+++ format/id/id_bench_test.go
@@ -6,6 +6,19 @@ import (
 	"github.com/eluv-io/common-go/format/id"
 )
 
+// Baseline performance:
+//
+// goos: darwin
+// goarch: amd64
+// pkg: github.com/eluv-io/common-go/format/id
+// cpu: Intel(R) Core(TM) i9-9980HK CPU @ 2.40GHz
+// BenchmarkId
+// BenchmarkId/string(id)
+// BenchmarkId/string(id)-16         	225890617	         4.986 ns/op
+// BenchmarkId/id.String()
+// BenchmarkId/id.String()-16        	 2496332	       443.5 ns/op
+// BenchmarkId/noop
+// BenchmarkId/noop-16               	1000000000	         0.2818 ns/op
 func BenchmarkId(b *testing.B) {
 	anID := id.Generate(id.User)
 	b.Run("string(id)", func(b *testing.B) {
