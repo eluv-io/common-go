9a5456f158191f657867a7b3bfbd03f4e5669fec
elv-gilles <gilles.gaillard@eluv.io>
2022-02-23 12:18:09 +0100

    auth: add grant 'read-metering' to allow reading metering information

--- format/eat/auth.go
+++ format/eat/auth.go
@@ -1,19 +1,21 @@
 package eat
 
-// grant is the type of grant kinds for authorization access
+// Grant is the type of grant kinds for authorization access
 type Grant string
 
 // Grants defines the kinds of access authorizations.
 var Grants = struct {
-	Create    Grant
-	Access    Grant
-	Read      Grant
-	Update    Grant
-	ReadCrypt Grant
+	Create       Grant
+	Access       Grant
+	Read         Grant
+	Update       Grant
+	ReadCrypt    Grant
+	ReadMetering Grant
 }{
-	Create:    "create",
-	Access:    "access",
-	Read:      "read",
-	Update:    "update",
-	ReadCrypt: "read-crypt", // read from content crypt
+	Create:       "create",
+	Access:       "access",
+	Read:         "read",
+	Update:       "update",
+	ReadCrypt:    "read-crypt", // read from content crypt
+	ReadMetering: "read-metering",
 }
