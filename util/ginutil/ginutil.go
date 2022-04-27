b8a8c3c5f75d00899c3bf7fa7cacfeee1fbfbc3e
Lukas Anliker <lukas.anliker@eluv.io>
2022-04-27 17:23:36 +0200

    add ginutil (extracted from qfab/daemon/utils)
    + move GetBytesRange to httputil
    + add unit tests

---
+++ util/ginutil/ginutil.go
@@ -0,0 +1,103 @@
+package ginutil
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+
+	"github.com/eluv-io/common-go/util/httputil"
+	"github.com/eluv-io/common-go/util/stackutil"
+	"github.com/eluv-io/errors-go"
+	"github.com/eluv-io/log-go"
+)
+
+// Abort aborts the current HTTP request with the given error. The HTTP status
+// code is set according to the error type.
+func Abort(c *gin.Context, err error) {
+	c.Abort()
+	code := http.StatusInternalServerError
+	if e, ok := err.(*errors.Error); ok {
+		switch e.Kind() {
+		case errors.K.Invalid, errors.K.Timeout, errors.K.Cancelled:
+			code = http.StatusBadRequest
+		case errors.K.Exist:
+			code = http.StatusConflict
+		case errors.K.NotExist:
+			code = http.StatusNotFound
+		case errors.K.Finalized:
+			code = http.StatusConflict
+		case errors.K.NotFinalized:
+			code = http.StatusConflict
+		case errors.K.Permission:
+			code = http.StatusForbidden
+		case errors.K.NoMediaMatch:
+			code = http.StatusNotAcceptable
+		case errors.K.Unavailable:
+			code = http.StatusServiceUnavailable
+		case errors.K.Other:
+			dumpGoRoutines()
+		}
+	} else {
+		dumpGoRoutines()
+	}
+	SendError(c, code, err)
+}
+
+// AbortWithStatus aborts the current HTTP request with the given status code
+// and error.
+func AbortWithStatus(c *gin.Context, code int, err error) {
+	c.Abort()
+	SendError(c, code, err)
+}
+
+func dumpGoRoutines() {
+	if !log.IsDebug() {
+		return
+	}
+	log.Error("dumping go-routines", "dump", stackutil.FullStack())
+}
+
+// SendError sends back an HTTP response with the given HTTP status code and the
+// data marshaled to JSON or XML depending on the "accept" headers of the
+// request. The data is marshaled to JSON if no accept headers are specified. No
+// data is marshaled if an accept headers other than 'application/json' or
+// 'application/xml' is specified.
+func SendError(c *gin.Context, code int, err error) {
+	if err != nil {
+		log.Debug("api error", "code", code, "error", err)
+	}
+	c.Writer.Header().Del("Content-Type")
+	c.Writer.Header().Del("Cache-Control")
+	switch c.NegotiateFormat(gin.MIMEJSON, gin.MIMEXML) {
+	case binding.MIMEJSON:
+		c.JSON(code, gin.H{"errors": []interface{}{err}})
+	case binding.MIMEXML:
+		c.Render(code, httputil.NewCustomXMLRenderer(gin.H{"errors": []interface{}{err}}))
+	default:
+		c.JSON(code, gin.H{"errors": []interface{}{err}})
+	}
+}
+
+// Send send back an HTTP response with the given HTTP status code and the data
+// marshaled to JSON or XML depending on the "accept" headers of the request.
+// The data is marshaled to JSON if no accept headers are specified.
+// If an incompatible accept header is specified an error is returned with code
+// '406 - Not Acceptable'
+func Send(c *gin.Context, code int, data interface{}) {
+	c.Writer.Header().Del("Content-Type")
+	switch c.NegotiateFormat(gin.MIMEJSON, gin.MIMEXML) {
+	case binding.MIMEJSON:
+		c.JSON(code, data)
+	case binding.MIMEXML:
+		c.Render(code, httputil.NewCustomXMLRenderer(data))
+	default:
+		if code <= 0 {
+			// this is also called from middleware.ErrorHandler with an explicit
+			// code -1 to not modify the code in the context (which is _logged_
+			// afterward)
+			return
+		}
+		_ = c.AbortWithError(http.StatusNotAcceptable, errors.Str("the accepted formats are not offered by the server"))
+	}
+}
---
+++ util/ginutil/ginutil_test.go
@@ -0,0 +1,195 @@
+package ginutil
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/eluv-io/common-go/util/jsonutil"
+	"github.com/eluv-io/errors-go"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	gin.SetMode(gin.ReleaseMode)
+}
+
+func TestAbort(t *testing.T) {
+	tests := []struct {
+		err      error
+		wantCode int
+	}{
+		{nil, 500},
+		{io.EOF, 500},
+		{errors.E("op"), 500},
+		{errors.E("op", errors.K.Invalid), 400},
+		{errors.E("op", errors.K.Cancelled), 400},
+		{errors.E("op", errors.K.Permission), 403},
+		{errors.E("op", errors.K.NotExist), 404},
+		{errors.E("op", errors.K.NoMediaMatch), 406},
+		{errors.E("op", errors.K.Exist), 409},
+		{errors.E("op", errors.K.Finalized), 409},
+		{errors.E("op", errors.K.NotFinalized), 409},
+		{errors.E("op", errors.K.NotFound), 500},
+		{errors.E("op", errors.K.IO), 500},
+		{errors.E("op", errors.K.AVInput), 500},
+		{errors.E("op", errors.K.AVProcessing), 500},
+		{errors.E("op", errors.K.NotImplemented), 500},
+		{errors.E("op", errors.K.Unavailable), 503},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.err), func(t *testing.T) {
+			w, c := testCtx(t)
+
+			Abort(c, tt.err)
+			require.Equal(t, tt.wantCode, w.Code)
+			require.Equal(
+				t,
+				jsonutil.MarshalCompactString(map[string]interface{}{"errors": []interface{}{tt.err}}),
+				w.Body.String(),
+			)
+		})
+	}
+}
+
+func TestAbortWithStatus(t *testing.T) {
+	fnJson := func(err error) string {
+		return jsonutil.MarshalCompactString(map[string]interface{}{"errors": []interface{}{err}})
+	}
+
+	tests := []struct {
+		err      error
+		code     int
+		accept   string
+		wantBody func(err error) string
+	}{
+		{nil, 500, "", fnJson},
+		{nil, 500, "application/json", fnJson},
+		{nil, 500, "application/custom", fnJson},
+		{nil, 500, "application/xml", func(err error) string {
+			return `<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <errors>
+    <error/>
+  </errors>
+</root>
+`
+		}},
+		{nil, 404, "application/json", fnJson},
+		{io.EOF, 500, "application/json", fnJson},
+		{errors.E("op"), 409, "application/json", fnJson},
+		{errors.E("op", errors.K.Unavailable), 404, "application/json", fnJson},
+		{errors.NoTrace("op"), 404, "application/json", fnJson},
+		{errors.NoTrace("op"), 404, "application/xml", func(err error) string {
+			return `<?xml version="1.0" encoding="UTF-8"?>
+<root>
+  <errors>
+    <error>
+      <kind>unclassified error</kind>
+      <op>op</op>
+    </error>
+  </errors>
+</root>
+`
+		}},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprint(tt.err), func(t *testing.T) {
+			w, c := testCtx(t)
+			if tt.accept != "" {
+				c.Request.Header.Set("Accept", tt.accept)
+			}
+
+			AbortWithStatus(c, tt.code, tt.err)
+			require.Equal(t, tt.code, w.Code)
+			require.Equal(
+				t,
+				tt.wantBody(tt.err),
+				w.Body.String(),
+			)
+		})
+	}
+}
+
+func TestSendError_Xml(t *testing.T) {
+	w, c := testCtx(t)
+	c.Request.Header.Set("Accept", "application/xml")
+
+	err := errors.E("op", errors.K.NotExist)
+	SendError(c, 404, err)
+
+	require.Equal(t, 404, w.Code)
+	require.Contains(t, w.Body.String(), "<kind>item does not exist</kind>")
+	require.Contains(t, w.Body.String(), "<op>op</op>")
+}
+
+func TestSend(t *testing.T) {
+	tests := []struct {
+		res      interface{}
+		code     int
+		accept   string
+		wantBody string
+		wantCode int
+	}{
+		{
+			res:      "This is the result",
+			code:     200,
+			accept:   "application/json",
+			wantBody: "\"This is the result\"",
+		},
+		{
+			res:      "This is the result",
+			code:     200,
+			accept:   "application/xml",
+			wantBody: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>This is the result</root>\n",
+		},
+		{
+			res:      "This is the result",
+			code:     406,
+			accept:   "application/custom",
+			wantBody: "",
+		},
+		{
+			res:      "This is the result",
+			code:     -1,
+			accept:   "application/custom",
+			wantBody: "",
+			wantCode: 200,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprint("code", tt.code, "accept", tt.accept), func(t *testing.T) {
+
+			w, c := testCtx(t)
+			c.Request.Header.Set("Accept", tt.accept)
+
+			Send(c, tt.code, tt.res)
+
+			wantCode := tt.wantCode
+			if wantCode == 0 {
+				wantCode = tt.code
+			}
+			require.Equal(t, wantCode, w.Code)
+			require.Equal(
+				t,
+				tt.wantBody,
+				w.Body.String(),
+			)
+		})
+	}
+}
+
+func testCtx(t *testing.T) (*httptest.ResponseRecorder, *gin.Context) {
+	var err error
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Request, err = http.NewRequest("GET", "http://127.0.0.1", nil)
+	require.NoError(t, err)
+	return w, c
+}
