e0d8d5037a8bdfe22b4d8d364c98de948423db76
elv-gilles <gilles.gaillard@eluv.io>
2023-10-03 13:38:02 +0200

    move temporary files to eluvio temp dir

--- util/testutil/testdir.go
+++ util/testutil/testdir.go
@@ -5,8 +5,10 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/eluv-io/log-go"
+	"github.com/eluv-io/utc-go"
 )
 
 // TestingT is an interface wrapper around *testing.T
@@ -38,14 +40,55 @@ func NewTestDir(t TestingT, prefix string) (path string, cleanup func()) {
 	return
 }
 
-// TestDir creates a new, unique test directory with the given prefix in default
-// temp directory of the platform. It returns the directory's path and a cleanup
-// function to remove the directory after the test.
+// eluvioTempDir return the path to the eluvio temp directory for tests:
+//   - if the environment variable 'ELUVIO_TEST_TMPDIR' is not defined or empty,
+//     the folder is in the temp directory of the platform under path 'eluvio/YY_MM_DD.
+//   - otherwise if the variable is an absolute path, it is used as is
+//   - otherwise it's a path relative to the temp directory of the platform
+//
+// Note that the resulting path length may matter. For example, the below endpoint
+// variable must not be larger than 103 (since max path length for ipc is 108 -
+// including 'unix:') when calling:
+//
+//	  net.ListenUnix(
+//		"unix",
+//		&net.UnixAddr{Net: "unix", Name: endpoint})
+func eluvioTempDir() string {
+	tmp, ok := os.LookupEnv("ELUVIO_TEST_TMPDIR")
+	if !ok || tmp == "" {
+		return filepath.Join(
+			os.TempDir(),
+			"eluvio",
+			strings.ReplaceAll(utc.Now().Format(time.DateOnly), "-", "_")[2:])
+	}
+	if filepath.IsAbs(tmp) {
+		return tmp
+	}
+	return filepath.Join(os.TempDir(), tmp)
+}
+
+// CreateTemp creates a temporary file in the same way as os.CreateTemp does, but
+// if dir is the empty string, the file is created in the eluvio temp directory.
+func CreateTemp(dir, suffix string) (*os.File, error) {
+	if dir == "" {
+		dir = eluvioTempDir()
+	}
+	return os.CreateTemp(dir, suffix)
+}
+
+// TestDir creates a new, unique test directory with the given prefix in eluvio
+// temp directory (see eluvioTempDir).
+// It returns the directory's path and a cleanup function to remove the directory
+// after the test.
 func TestDir(prefix string) (path string, cleanup func()) {
-	path, err := os.MkdirTemp(os.TempDir(), prefix)
+	dir := eluvioTempDir()
+	_ = os.MkdirAll(dir, os.ModePerm)
+
+	path, err := os.MkdirTemp(dir, prefix)
 	if err != nil {
 		log.Fatal("failed to create test dir", err, "path", path)
 	}
+
 	// log.Info("test dir", "path", path)
 	cleanup = func() {
 		Purge(path)
