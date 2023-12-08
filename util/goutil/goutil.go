46b1652e85f8545747dd1eb09e1abb734568899b
Jonathan Ching <38669839+jslching@users.noreply.github.com>
2023-12-08 10:55:12 -0500

    add goutil.Log to log goroutine info (#34)

--- util/goutil/goutil.go
+++ util/goutil/goutil.go
@@ -1,8 +1,27 @@
 package goutil
 
-import "github.com/modern-go/gls"
+import (
+	"github.com/modern-go/gls"
+
+	elog "github.com/eluv-io/log-go"
+)
+
+var log = elog.Get("/eluvio/util/goutil")
 
 // GoID returns the goroutine id of current goroutine
 func GoID() int64 {
 	return gls.GoID()
+}
+
+// Log logs entry and exit info for the current goroutine
+// Example usage:
+//     defer goutil.Log("testFn", parentGID, "id", id.String())()
+func Log(name string, parentGoID int64, fields ...interface{}) func() {
+	if parentGoID > 0 {
+		fields = append(fields, "parent_gid", parentGoID)
+	}
+	log.Debug("goroutine.enter "+name, fields...)
+	return func() {
+		log.Debug("goroutine.exit "+name, fields...)
+	}
 }
