69bc0c5b6bce112e9ed067659300eb56605d2ee5
Lukas Anliker <lukseven@users.noreply.github.com>
2025-09-04 19:44:02 +0200

    add InitLogTracing & TraceLocker to traceutil, fix bugs in ctxutil (#79)
    * add InitLogTracing to initialize tracing based on a logger's log level
    * add TraceLocker for mutex tracing
    * fix bugs in context stack span handling

---
+++ util/traceutil/tracelocker_test.go
@@ -0,0 +1,138 @@
+package traceutil
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/eluv-io/common-go/util/jsonutil"
+	"github.com/eluv-io/utc-go"
+)
+
+func TestTraceLocker(t *testing.T) {
+	rootSpan := InitTracing("test", false)
+
+	locker := BuildTraceLocker("locktest").Build()
+
+	go func() {
+		// trace here is not recorded, since this goroutine is not initialized for tracing
+		defer locker.LockUnlock()()
+		time.Sleep(100 * time.Millisecond)
+	}()
+
+	time.Sleep(50 * time.Millisecond)
+	locker.Lock() // should block for ~50ms
+	locker.Unlock()
+
+	span := rootSpan.FindByName("locktest")
+	require.NotNil(t, span)
+
+	require.NotEmpty(t, span.Events())
+	require.Equal(t, "locked", span.Events()[0].Name)
+	require.InDelta(t, 50*time.Millisecond, span.Duration(), float64(20*time.Millisecond))
+
+	rootSpan.End()
+	fmt.Println(rootSpan.Json())
+}
+
+func ExampleTraceLocker() {
+	locker := BuildTraceLocker("example-lock").Build()
+	useLocker(locker, false)
+
+	// Output:
+	//
+	// {
+	//   "name": "root",
+	//   "time": "6s",
+	//   "subs": [
+	//     {
+	//       "name": "example-lock",
+	//       "time": "5s",
+	//       "attr": {
+	//         "caller": "traceutil.useLocker (tracelocker_test.go:129)"
+	//       },
+	//       "evnt": [
+	//         {
+	//           "name": "locked",
+	//           "at": "0s"
+	//         }
+	//       ]
+	//     }
+	//   ]
+	// }
+}
+
+func ExampleTraceLockerBuilder() {
+	locker := BuildTraceLocker("example-lock").WithCaller(false).WithSpanUnlock(false).Build()
+	useLocker(locker, false)
+
+	// Output:
+	//
+	// {
+	//   "name": "root",
+	//   "time": "6s",
+	//   "subs": [
+	//     {
+	//       "name": "example-lock",
+	//       "time": "0s",
+	//       "evnt": [
+	//         {
+	//           "name": "locked",
+	//           "at": "0s"
+	//         }
+	//       ]
+	//     }
+	//   ]
+	// }
+}
+
+func ExampleTraceLocker_LockUnlock() {
+	locker := BuildTraceLocker("example-lock").Build()
+	useLocker(locker, true)
+
+	// Output:
+	//
+	// {
+	//   "name": "root",
+	//   "time": "6s",
+	//   "subs": [
+	//     {
+	//       "name": "example-lock",
+	//       "time": "5s",
+	//       "attr": {
+	//         "caller": "traceutil.useLocker (tracelocker_test.go:125)"
+	//       },
+	//       "evnt": [
+	//         {
+	//           "name": "locked",
+	//           "at": "0s"
+	//         }
+	//       ]
+	//     }
+	//   ]
+	// }
+}
+
+func useLocker(locker *TraceLocker, lockUnlock bool) {
+	now := utc.UnixMilli(0)
+	defer utc.MockNowFn(func() utc.UTC { return now })()
+
+	rootSpan := InitTracing("root", false)
+
+	if lockUnlock {
+		unlock := locker.LockUnlock()
+		now = now.Add(5 * time.Second)
+		unlock()
+	} else {
+		locker.Lock()
+		now = now.Add(5 * time.Second)
+		locker.Unlock()
+	}
+
+	now = now.Add(1 * time.Second)
+	rootSpan.End()
+
+	fmt.Println(jsonutil.MustPretty(rootSpan.Json()))
+}
