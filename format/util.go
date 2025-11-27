691438fd76a61de9012fae00dc7c5ec6a1ca6003
Lukas Anliker <lukas.anliker@eluv.io>
2025-11-27 22:08:11 +0100

    add format.ParseQihot & format.ParseQhot

--- format/util.go
+++ format/util.go
@@ -9,20 +9,62 @@ import (
 
 // ExtractQID tries to extract a content ID from the given content hash, id or write token string. Returns nil if no
 // content ID is found.
-func ExtractQID(qhit string) types.QID {
-	qid, err := id.Q.FromString(qhit)
+func ExtractQID(qihot string) types.QID {
+	qid, err := id.Q.FromString(qihot)
 	if err == nil {
 		return qid
 	}
 
-	hsh, err := hash.Q.FromString(qhit)
+	hsh, err := hash.Q.FromString(qihot)
 	if err == nil {
 		return hsh.ID
 	}
 
-	qwt, err := token.QWrite.FromString(qhit)
+	qwt, err := token.QWrite.FromString(qihot)
 	if err == nil {
 		return qwt.QID
 	}
 	return nil
+}
+
+// ParseQhot parses the given string as a content hash or write token. Returns
+//   - (id, hash, nil) if the string is a content hash
+//   - (id, nil, token) if it is a write token
+//   - (nil, nil, nil) if it is neither.
+func ParseQhot(qhot string) (types.QID, types.QHash, types.QWriteToken) {
+	hsh, err := hash.Q.FromString(qhot)
+	if err == nil {
+		return hsh.ID, hsh, nil
+	}
+
+	qwt, err := token.QWrite.FromString(qhot)
+	if err == nil {
+		return qwt.QID, nil, qwt
+	}
+
+	return nil, nil, nil
+}
+
+// ParseQihot parses the given string as a content hash or write token. Returns
+//   - (id, nil, nil) if the string is a content id
+//   - (id, hash, nil) if the string is a content hash
+//   - (id, nil, token) if the string is a write token
+//   - (nil, nil, nil) if the string is neither
+func ParseQihot(qihot string) (types.QID, types.QHash, types.QWriteToken) {
+	qid, err := id.Q.FromString(qihot)
+	if err == nil {
+		return qid, nil, nil
+	}
+
+	hsh, err := hash.Q.FromString(qihot)
+	if err == nil {
+		return hsh.ID, hsh, nil
+	}
+
+	qwt, err := token.QWrite.FromString(qihot)
+	if err == nil {
+		return qwt.QID, nil, qwt
+	}
+
+	return nil, nil, nil
 }
--- format/util_test.go
+++ format/util_test.go
@@ -28,3 +28,69 @@ func TestExtractQID(t *testing.T) {
 		})
 	}
 }
+
+func TestParseQihot(t *testing.T) {
+	tests := []struct {
+		qihot   string
+		wantQid types.QID
+		wantHsh types.QHash
+		wantTok types.QWriteToken
+	}{
+		{"", nil, nil, nil},
+		{"abcd", nil, nil, nil},
+		{"iq__48iLSSjzN3PRyzwWqDmG5Dx1zkfL", id.MustParse("iq__48iLSSjzN3PRyzwWqDmG5Dx1zkfL"), nil, nil},
+		{
+			"hq__EKjpzYq4vjPxchdoSm8fUSvK2y3PYVgLPdMWP8yqRRvu4rBnv3BY1BS7pdjVjfvvsasaTZA9qq",
+			id.MustParse("iq__3MRbyPWE1EwEnPb2uNgVPHgF57Qj"),
+			hash.MustParse("hq__EKjpzYq4vjPxchdoSm8fUSvK2y3PYVgLPdMWP8yqRRvu4rBnv3BY1BS7pdjVjfvvsasaTZA9qq"),
+			nil,
+		},
+		{
+			"tqw__8UmhDD9cZah58THfAYPf3Shj9hVzfwT51Cf4ZHKpayajzZRyMwCPiSpfS5yqRZfjkDjrtXuRmDa",
+			id.MustParse("iq__99d4kp14eSDEP7HWfjU4W6qmqDw"),
+			nil,
+			token.MustParse("tqw__8UmhDD9cZah58THfAYPf3Shj9hVzfwT51Cf4ZHKpayajzZRyMwCPiSpfS5yqRZfjkDjrtXuRmDa"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.qihot, func(t *testing.T) {
+			qid, hsh, tok := ParseQihot(tt.qihot)
+			assert.Equal(t, tt.wantQid, qid)
+			assert.Equal(t, tt.wantHsh, hsh)
+			assert.Equal(t, tt.wantTok, tok)
+		})
+	}
+}
+
+func TestParseQhot(t *testing.T) {
+	tests := []struct {
+		qhot    string
+		wantQid types.QID
+		wantHsh types.QHash
+		wantTok types.QWriteToken
+	}{
+		{"", nil, nil, nil},
+		{"abcd", nil, nil, nil},
+		{"iq__48iLSSjzN3PRyzwWqDmG5Dx1zkfL", nil, nil, nil},
+		{
+			"hq__EKjpzYq4vjPxchdoSm8fUSvK2y3PYVgLPdMWP8yqRRvu4rBnv3BY1BS7pdjVjfvvsasaTZA9qq",
+			id.MustParse("iq__3MRbyPWE1EwEnPb2uNgVPHgF57Qj"),
+			hash.MustParse("hq__EKjpzYq4vjPxchdoSm8fUSvK2y3PYVgLPdMWP8yqRRvu4rBnv3BY1BS7pdjVjfvvsasaTZA9qq"),
+			nil,
+		},
+		{
+			"tqw__8UmhDD9cZah58THfAYPf3Shj9hVzfwT51Cf4ZHKpayajzZRyMwCPiSpfS5yqRZfjkDjrtXuRmDa",
+			id.MustParse("iq__99d4kp14eSDEP7HWfjU4W6qmqDw"),
+			nil,
+			token.MustParse("tqw__8UmhDD9cZah58THfAYPf3Shj9hVzfwT51Cf4ZHKpayajzZRyMwCPiSpfS5yqRZfjkDjrtXuRmDa"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.qhot, func(t *testing.T) {
+			qid, hsh, tok := ParseQhot(tt.qhot)
+			assert.Equal(t, tt.wantHsh, hsh)
+			assert.Equal(t, tt.wantTok, tok)
+			assert.Equal(t, tt.wantQid, qid, "%s", qid)
+		})
+	}
+}
