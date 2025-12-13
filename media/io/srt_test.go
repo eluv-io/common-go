5302fc9d52c170afa6c54953b16a62352e2b16fe
Lukas Anliker <lukas.anliker@eluv.io>
2025-12-13 03:25:45 +0100

    add missed unit test for srt source/sink

---
+++ media/io/srt_test.go
@@ -0,0 +1,124 @@
+package io
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/eluv-io/common-go/util/testutil"
+)
+
+func TestSourceSink(t *testing.T) {
+	log.SetDebug()
+
+	tests := []struct {
+		proto          string
+		connectionLess bool
+	}{
+		{"udp", true},
+		{"rtp", true},
+		{"srt", false},
+	}
+	for _, test := range tests {
+		t.Run(test.proto, func(t *testing.T) {
+			for _, host := range []string{
+				"127.0.0.1",
+				"[::1]", // ==> not sure it's working on github actions
+				// "localhost",  ==> doesn't work for srt!
+			} {
+				t.Run(host, func(t *testing.T) {
+					testSourceSink(t, host, test.proto, test.connectionLess)
+				})
+			}
+		})
+	}
+}
+
+func testSourceSink(t *testing.T, host, proto string, connectionLess bool) {
+	port, err := testutil.FreePort()
+	require.NoError(t, err)
+
+	// create the source
+	source, err := CreatePacketSource(fmt.Sprintf("%s://%s:%d?mode=listen", proto, host, port))
+	require.NoError(t, err)
+	reader, err := source.Open()
+	require.NoError(t, err)
+	defer log.Call(reader.Close, "close source")
+
+	// allow some time for the listener to start
+	time.Sleep(100 * time.Millisecond)
+
+	// connect the sink
+	sink, err := CreatePacketSink(fmt.Sprintf("%s://%s:%d", proto, host, port))
+	require.NoError(t, err)
+	writer, err := sink.Open()
+	require.NoError(t, err)
+	defer log.Call(writer.Close, "close sink")
+
+	// write to the sink
+	n, err := writer.Write([]byte{1, 2, 3})
+	require.NoError(t, err)
+	require.Equal(t, 3, n)
+
+	// read from the source
+	packet := make([]byte, 1024)
+	n, err = reader.Read(packet)
+	require.NoError(t, err)
+	require.Equal(t, 3, n)
+
+	require.Equal(t, []byte{1, 2, 3}, packet[:n])
+}
+
+func TestSourceCreationErrors(t *testing.T) {
+	tests := []struct {
+		url       string
+		createErr string
+		openErr   string
+	}{
+		{"udp://invalid_address:8080", "", "no such host"},
+		{"rtp://invalid_address:8080", "", "no such host"},
+		{"srt://invalid_address:8080", "", "failed dialing"},
+		{"http://invalid_address:8080", "unsupported protocol", ""},
+	}
+
+	for _, test := range tests {
+		t.Run(test.url, func(t *testing.T) {
+			src, err := CreatePacketSource(test.url)
+			if test.createErr != "" {
+				require.ErrorContains(t, err, test.createErr)
+				return
+			}
+			require.NoError(t, err)
+			_, err = src.Open()
+			require.ErrorContains(t, err, test.openErr)
+		})
+	}
+}
+
+func TestSinkCreationErrors(t *testing.T) {
+	tests := []struct {
+		url       string
+		createErr string
+		openErr   string
+	}{
+		{"udp://invalid_address:8080", "", "no such host"},
+		{"rtp://invalid_address:8080", "", "no such host"},
+		{"srt://invalid_address:8080", "", "failed dialing"},
+		{"http://invalid_address:8080", "unsupported protocol", ""},
+	}
+
+	for _, test := range tests {
+		t.Run(test.url, func(t *testing.T) {
+			sink, err := CreatePacketSink(test.url)
+			if test.createErr != "" {
+				require.ErrorContains(t, err, test.createErr)
+				return
+			}
+			require.NoError(t, err)
+			_, err = sink.Open()
+			require.ErrorContains(t, err, test.openErr)
+		})
+	}
+}
