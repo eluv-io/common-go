c41e65c3410e78b293564d4bda13a03380ad3527
jslching <38669839+jslching@users.noreply.github.com>
2026-02-11 14:03:09 -0500

    add ts adjust stats

--- media/rtp/rtp_stream_tracker.go
+++ media/rtp/rtp_stream_tracker.go
@@ -45,7 +45,8 @@ type rtpStreamTracker struct {
 	logThrottle timeutil.Periodic
 	panics      int
 	detector    *GapDetector
-	wallClockT0 utc.UTC
+	currT0      utc.UTC
+	origT0      utc.UTC
 }
 
 func (t *rtpStreamTracker) Track(bts []byte) (payload []byte, timestamp utc.UTC, errList error) {
@@ -105,11 +106,23 @@ func (t *rtpStreamTracker) Reset() {
 }
 
 func (t *rtpStreamTracker) toWallClockTS(rtpTS int64) utc.UTC {
-	wallClockT0 := utc.Now().Add(TicksToDuration(rtpTS) * -1)
-	if t.wallClockT0.IsZero() || wallClockT0.Before(t.wallClockT0) {
-		t.wallClockT0 = wallClockT0
+	// A RTP timestamp is the number of ticks since the stream start, which is designated as RTP timestamp zero.
+	// The wall clock timestamp of stream start, called T0, is calculated by subtracting a RTP timestamp (converted to
+	// a time duration) from the current wall clock time. So, the calculated T0 depends on the receive time of a packet.
+	// To improve the approximation of T0, calculate T0 for each new packet received and take the earliest T0 found.
+	// The wall clock timestamp for a given packet is then calculated by adding the RTP timestamp (converted to a time
+	// duration) to the current T0.
+	rtpDur := TicksToDuration(rtpTS)
+	t0 := utc.Now().Add(rtpDur * -1)
+	if t.origT0.IsZero() {
+		t.currT0 = t0
+		t.origT0 = t0
+	} else if t0.Before(t.currT0) {
+		t.currT0 = t0
+		t.stats.TsAdjCount++
+		t.stats.TsAdjDuration = duration.Spec(t.origT0.Sub(t.currT0))
 	}
-	return t.wallClockT0.Add(TicksToDuration(rtpTS))
+	return t.currT0.Add(rtpDur)
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -129,16 +142,18 @@ func (n NoopTracker) Reset() {}
 // ---------------------------------------------------------------------------------------------------------------------
 
 type Stats struct {
-	Start       utc.UTC       `json:"start"`
-	Duration    duration.Spec `json:"duration"`
-	PacketCount int           `json:"packet_count"`
-	ErrorCount  int           `json:"error_count"`
-	StartSeq    int64         `json:"start_seq"`
-	EndSeq      int64         `json:"end_seq"`
-	StartTs     int64         `json:"start_ts"`
-	EndTs       int64         `json:"end_ts"`
-	RtpDuration duration.Spec `json:"rtp_duration"`
-	Gaps        []Gap         `json:"gaps"`
+	Start         utc.UTC       `json:"start"`
+	Duration      duration.Spec `json:"duration"`
+	PacketCount   int           `json:"packet_count"`
+	ErrorCount    int           `json:"error_count"`
+	StartSeq      int64         `json:"start_seq"`
+	EndSeq        int64         `json:"end_seq"`
+	StartTs       int64         `json:"start_ts"`
+	EndTs         int64         `json:"end_ts"`
+	RtpDuration   duration.Spec `json:"rtp_duration"`
+	TsAdjCount    int           `json:"ts_adj_count"`
+	TsAdjDuration duration.Spec `json:"ts_adj_duration"`
+	Gaps          []Gap         `json:"gaps"`
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
