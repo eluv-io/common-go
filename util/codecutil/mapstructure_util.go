4dff31a26227049122232e3d778f0a7f042c9415
elv-gilles <40674218+elv-gilles@users.noreply.github.com>
2026-06-02 15:17:54 +0200

    add optional parameter 'squash' to codecutil.MapDecode (#87)
    * add optional parameter 'squash' to codecutil.MapDecode
    * upgrade to mapstructure v1.5.0 - forked for performance improvement -> v1.5.1
    * improve performance in MapDecode by not using the reflect package when casting is possible
    * update go.mod for eluv-io/mapstructure (mitchellh/mapstructure was repackaged into eluv-io)

--- util/codecutil/mapstructure_util.go
+++ util/codecutil/mapstructure_util.go
@@ -5,7 +5,7 @@ import (
 	"encoding/base64"
 	"reflect"
 
-	"github.com/mitchellh/mapstructure"
+	"github.com/eluv-io/mapstructure"
 )
 
 type MapUnmarshaler interface {
@@ -25,16 +25,23 @@ var textUnmarshaler = reflect.TypeOf((*encoding.TextUnmarshaler)(nil)).Elem()
 // `json:...` tags defined on the destination structure's member fields will be
 // used for unmarshaling (just like when unmarshaling JSON text).
 //
-// The implementation uses github.com/mitchellh/mapstructure to do the decoding,
+// The implementation uses github.com/eluv-io/mapstructure to do the decoding,
 // with the following special decoding hooks:
 //   - decodes with the 'UnmarshalMap(m map[string]interface{}) error'
 //     function if implemented by the destination object/field
 //   - decodes with the 'UnmarshalText(text []byte) error' function if the
 //     destination implements encoding.TextUnmarshaler
-func MapDecode(src interface{}, dst interface{}) error {
+// When the variadic squash parameter is used, its first value is set to the
+// Squash field of the decoder configuration.
+func MapDecode(src interface{}, dst interface{}, squash ...bool) error {
+	sqsh := false
+	if len(squash) > 0 {
+		sqsh = squash[0]
+	}
 	cfg := &mapstructure.DecoderConfig{
 		TagName:    "json",
 		Result:     dst,
+		Squash:     sqsh,
 		DecodeHook: decodeHook,
 	}
 	decoder, err := mapstructure.NewDecoder(cfg)
@@ -52,29 +59,27 @@ func decodeHook(f reflect.Type, t reflect.Type, data interface{}) (interface{}, 
 		t, ptr := resolve(t)
 		if ptr.Implements(mapUnmarshaler) {
 			instance := reflect.New(t)
-			fnc := instance.MethodByName("UnmarshalMap")
-			if !fnc.IsValid() {
-				return data, nil
+
+			ret := instance.Interface()
+			err := ret.(MapUnmarshaler).UnmarshalMap(dt)
+			if err != nil {
+				return nil, err
 			}
-			err := fnc.Call([]reflect.Value{reflect.ValueOf(dt)})
-			if len(err) > 0 && !err[0].IsNil() {
-				return nil, err[0].Interface().(error)
-			}
-			return instance.Interface(), nil
+			return ret, nil
+
 		}
 	case string:
 		t, ptr := resolve(t)
 		if ptr.Implements(textUnmarshaler) {
 			instance := reflect.New(t)
-			fnc := instance.MethodByName("UnmarshalText")
-			if !fnc.IsValid() {
-				return data, nil
+
+			ret := instance.Interface()
+			err := ret.(encoding.TextUnmarshaler).UnmarshalText([]byte(dt))
+			if err != nil {
+				return nil, err
 			}
-			err := fnc.Call([]reflect.Value{reflect.ValueOf([]byte(dt))})
-			if len(err) > 0 && !err[0].IsNil() {
-				return nil, err[0].Interface().(error)
-			}
-			return instance.Interface(), nil
+			return ret, nil
+
 		} else if t == byteSliceType {
 			// byte arrays are marshaled to base64 encoded string in JSON by default...
 			return base64.StdEncoding.DecodeString(dt)
@@ -88,6 +93,6 @@ func resolve(t reflect.Type) (reflect.Type, reflect.Type) {
 	for t.Kind() == reflect.Ptr {
 		t = t.Elem()
 	}
-	ptr := reflect.PtrTo(t)
+	ptr := reflect.PointerTo(t)
 	return t, ptr
 }
